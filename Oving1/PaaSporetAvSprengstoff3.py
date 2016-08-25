3145b128acdfb8e119ede6bde853acbdacd9b59c
Håvard Aspen Løvik <hloevi@live.no>
2016-08-25 22:06:16 +0200

    best way

--- Oving1/PaaSporetAvSprengstoff3.py
+++ Oving1/PaaSporetAvSprengstoff3.py
@@ -1,6 +1,2 @@
 from sys import stdin
-import time
-start = time.clock()
-data = [int(i) for i in stdin.readlines()]
-end = time.clock()
-print(max(data),"time(s): "+str(end-start))
+print(max(int(i) for i in stdin.readlines()))
