2291d14de627c5b5ba67411e1caa1c3d1ca05a5e
Håvard Aspen Løvik <hloevi@live.no>
2016-10-09 23:19:39 +0200

    rod-cut and Oving7

---
+++ tests/rodCut.py
@@ -0,0 +1,18 @@
+from sys import stdin
+A = []
+for tall in stdin:
+    A.append(int(tall))
+
+
+def cut_rod(p, n):
+    r = [0] * (n+1)
+    for i in range(1,n+1):
+      q = float("-inf")
+      for j in range(1,i+1):
+        q = max(q,p[j] + r[i - j])
+      r[i] = q
+    return r[n]
+
+
+print(cut_rod(A,7))
+
